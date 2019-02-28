e05b224628b111ea9a7bb6481a67c6162c2bbf33
durham-abric <durham.abric@mail.mcgill.ca>
2019-02-27 22:26:44 -0500

    Mutual Friend Query

--- db_queries/sp_getMutualFriends.sql
+++ db_queries/sp_getMutualFriends.sql
@@ -1,23 +1,19 @@
-DELIMITER $$
-
 CREATE OR REPLACE PROCEDURE getMutualFriends(
     IN user1 INTEGER,
-    IN user2 INTEGER
-)
-LANGUAGE SQL
-DYNAMIC RESULT SETS 1
+    IN user2 INTEGER)
+    LANGUAGE SQL
+    DYNAMIC RESULT SETS 1
 
-BEGIN
+    C1: BEGIN
   
-    DECLARE c CURSOR WITH RETURN TO CLIENT
-        FOR SELECT DISTINCT(USERB) FROM BIDIRECTIONALFRIENDS AS bdf1
-                WHERE EXISTS(SELECT * FROM BIDIRECTIONALFRIENDS as bdf2 
+        DECLARE c CURSOR WITH RETURN TO CLIENT
+            FOR SELECT DISTINCT(USERB) FROM BIDIRECTIONALFRIENDS AS bdf1
+                    WHERE EXISTS(SELECT * FROM BIDIRECTIONALFRIENDS as bdf2 
                                 WHERE bdf1.USERA = user1 
                                 AND bdf2.USERB = user2 
                                 AND bdf1.USERB = bdf2.USERB);
     
-    OPEN c;
-    
-END $$
+        OPEN c;
 
-DELIMITER ;
+    END C1;
+
