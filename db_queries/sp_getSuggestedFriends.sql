574b711660c35c5dd53000ea2653b7679fbb73c9
durham-abric <durham.abric@mail.mcgill.ca>
2019-02-28 10:25:06 -0500

    Refactor of getSuggestedFriends(...)

--- db_queries/sp_getSuggestedFriends.sql
+++ db_queries/sp_getSuggestedFriends.sql
@@ -1,10 +1,25 @@
 CREATE OR REPLACE PROCEDURE getSuggestedFriends(IN user1 INTEGER)
     LANGUAGE SQL
     BEGIN
-        DECLARE cur CURSOR WITH RETURN TO CALLER
-            FOR SELECT DISTINCT(USERB) 
-                    FROM FRIENDSOFFRIENDS
-                WHERE USERA = user1
-                ORDER BY COUNT(CALL getMutualFriends(user1, USERB) DESC;
-        OPEN cur;
+        CREATE Table TempSuggested(
+            suggestion INTEGER UNIQUE NOT NULL PRIMARY KEY,
+            mutualFriends INTEGER NOT NULL DEFAULT 0,
+            FOREIGN KEY (suggestion) REFERENCES User(ownID)
+        );
+
+        INSERT INTO TempSuggested(suggestion)
+        SELECT USERB
+            FROM FRIENDSOFFRIENDS
+        WHERE USERA = user1;
+
+        INSERT INTO TempSuggested(mutualFriends)
+        SELECT COUNT(CALL sp_getMutualFriends(user1, ts.suggestion))
+            FROM TempSuggested as ts
+        WHERE suggestion = ts.suggestion;
+
+        SELECT * 
+            FROM TempSuggested
+        ORDER BY mutualFriends DESC;
+
+        DROP TABLE IF EXISTS TempSuggested;
     END@
