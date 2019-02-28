715788c0fd60fd5971b33e180ca658dde4eefbfd
durham-abric <durham.abric@mail.mcgill.ca>
2019-02-28 17:05:11 -0500

    All queries made into SPs

--- db_queries/sp_getSuggestedEvents.sql
+++ db_queries/sp_getSuggestedEvents.sql
@@ -5,8 +5,8 @@ CREATE OR REPLACE PROCEDURE getSuggestedEvents(IN user1 INTEGER)
 
         DECLARE cur CURSOR WITH RETURN TO CALLER
             FOR SELECT eid, eventName, eventDate, location
-                    FROM EVENT AS ev
-                WHERE numFriendsAttending(user1, ev.eid) > 0
+                    FROM EVENT
+                WHERE numFriendsAttending(user1, eid) > 0
                 AND eventDate > (SELECT current date FROM sysibm.sysdummy1)
                 ORDER BY numFriendsAttending(user1, eid) DESC;
         
