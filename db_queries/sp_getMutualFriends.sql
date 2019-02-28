7dbae598f4831cdb295e68be12f48b43fd932912
durham-abric <durham.abric@mail.mcgill.ca>
2019-02-28 08:43:27 -0500

    Query working - stored procedure creation not

--- db_queries/sp_getMutualFriends.sql
+++ db_queries/sp_getMutualFriends.sql
@@ -1,22 +1,22 @@
---CREATE OR REPLACE PROCEDURE getMutualFriends(
---    IN user1 INTEGER,
---    IN user2 INTEGER)
---    LANGUAGE SQL
---    DYNAMIC RESULT SETS 1
+CREATE OR REPLACE PROCEDURE getMutualFriends(
+    IN user1 INTEGER,
+    IN user2 INTEGER)
+    LANGUAGE SQL
+    DYNAMIC RESULT SETS 1
 
---    C1: BEGIN
+    C1: BEGIN
   
---        DECLARE c CURSOR WITH RETURN TO CLIENT
---            FOR
+        DECLARE c CURSOR WITH RETURN TO CLIENT
+            FOR
 
 
                 SELECT DISTINCT(USERB) FROM BIDIRECTIONALFRIENDS AS bdf1
                     WHERE EXISTS(SELECT * FROM BIDIRECTIONALFRIENDS as bdf2 
-                                WHERE bdf1.USERA = 1 
-                                AND bdf2.USERB = 2 
+                                WHERE bdf1.USERA = user1 
+                                AND bdf2.USERA = user2 
                                 AND bdf1.USERB = bdf2.USERB);
     
---        OPEN c;
+        OPEN c;
 
---    END C1;
+    END C1;
 
