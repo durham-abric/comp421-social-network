a95dda5c65c7b8f4a09b8aaa131b711a68cd87ae
durham-abric <durham.abric@mail.mcgill.ca>
2019-02-28 13:48:59 -0500

    Suggested friends procedure complete.

--- db_queries/sp_getSuggestedFriends.sql
+++ db_queries/sp_getSuggestedFriends.sql
@@ -1,31 +1,15 @@
 CREATE OR REPLACE PROCEDURE getSuggestedFriends(IN user1 INTEGER)
     LANGUAGE SQL
 
-    P1: BEGIN
-
-        CREATE Table TempSuggested(
-            suggestion INTEGER UNIQUE NOT NULL PRIMARY KEY,
-            mutualFriends BIGINT NOT NULL DEFAULT 0,
-            FOREIGN KEY (suggestion) REFERENCES User(ownID)
-        );
-
-        INSERT INTO TempSuggested(suggestion)
-        SELECT USERB
-            FROM FRIENDSOFFRIENDS
-        WHERE USERA = user1;
-
-        UPDATE TempSuggested
-        SET mutualFriends = 
-            (SELECT COUNT(*)
-                FROM TABLE(getMutualFriends(user1, suggestion)));
+    BEGIN ATOMIC
 
         DECLARE cur CURSOR WITH RETURN TO CALLER
-            FOR SELECT * 
-                    FROM TempSuggested
-                ORDER BY mutualFriends DESC;
-
+            FOR SELECT USERB
+                    FROM FRIENDSOFFRIENDS
+                WHERE USERA = user1
+                ORDER BY (SELECT COUNT(*)
+                            FROM TABLE(getMutualFriends(user1, suggestion)));
+       
         OPEN cur;
 
-        DROP TABLE TempSuggested;
-
-    END P1@
+    END@
