11fdf5ab28a1fb7c51891aba62543fa32f624dd9
durham-abric <durham.abric@mail.mcgill.ca>
2019-02-28 13:22:14 -0500

    Suggested friends procedure complete.

--- db_queries/sp_getSuggestedFriends.sql
+++ db_queries/sp_getSuggestedFriends.sql
@@ -5,7 +5,9 @@ CREATE OR REPLACE PROCEDURE getSuggestedFriends(IN user1 INTEGER)
 
         DECLARE cur CURSOR WITH RETURN TO CALLER
             FOR SELECT USERB, COUNT(TABLE(getMutualFriends(user1, USERB))) AS numMutual
-                    FROM FRIENDSOFFRIENDS
+                    FROM FRIENDSOFFRIENDS as fof
+                    JOIN TABLE(getMutualFriends(user1, USERB)) as mut
+                        ON USERB = mut.mutualFriend
                 WHERE USERA = user1
                 ORDER BY numMutual DESC;
         
