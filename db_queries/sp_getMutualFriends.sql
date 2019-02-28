fa1b5158102cf6b6298f3f96cb49e3197e7419a1
durham-abric <durham.abric@mail.mcgill.ca>
2019-02-28 09:05:15 -0500

    Edits

--- db_queries/sp_getMutualFriends.sql
+++ db_queries/sp_getMutualFriends.sql
@@ -3,16 +3,16 @@ CREATE OR REPLACE PROCEDURE getMutualFriends(IN user1 INTEGER, IN user2 INTEGER)
     AS
     BEGIN
   
-        --DECLARE cur CURSOR WITH RETURN TO CLIENT
-            --FOR
+        DECLARE cur CURSOR WITH RETURN TO CLIENT
+            FOR
             SELECT DISTINCT(USERB) 
                     FROM BIDIRECTIONALFRIENDS AS bdf1
                 WHERE EXISTS(SELECT * 
                                 FROM BIDIRECTIONALFRIENDS as bdf2 
                             WHERE bdf1.USERA = user1 AND
                                 bdf2.USERA = user2 AND
-                                bdf1.USERB = bdf2.USERB)
+                                bdf1.USERB = bdf2.USERB);
     
-        --OPEN cur;
+        OPEN cur;
 
-    END
+    END@
