9aa8eda888dfc0eb9a2f51b389e5f77e7ae0ce36
durham-abric <durham.abric@mail.mcgill.ca>
2019-02-28 09:32:13 -0500

    Suggested friends procedure added.

--- db_queries/sp_getMutualFriends.sql
+++ db_queries/sp_getMutualFriends.sql
@@ -1,9 +1,7 @@
 CREATE OR REPLACE PROCEDURE getMutualFriends(IN user1 INTEGER, IN user2 INTEGER)
     LANGUAGE SQL
-    AS
     BEGIN
-  
-        DECLARE cur CURSOR WITH RETURN TO CLIENT
+        DECLARE cur CURSOR WITH RETURN TO CALLER
             FOR
             SELECT DISTINCT(USERB) 
                     FROM BIDIRECTIONALFRIENDS AS bdf1
@@ -12,7 +10,5 @@ CREATE OR REPLACE PROCEDURE getMutualFriends(IN user1 INTEGER, IN user2 INTEGER)
                             WHERE bdf1.USERA = user1 AND
                                 bdf2.USERA = user2 AND
                                 bdf1.USERB = bdf2.USERB);
-    
         OPEN cur;
-
     END@
---
+++ db_queries/sp_getSuggestedFriends.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE PROCEDURE getSuggestedFriends(IN user1 INTEGER)
+    LANGUAGE SQL
+    BEGIN
+        DECLARE cur CURSOR WITH RETURN TO CALLER
+            FOR
+            SELECT DISTINCT(USERB) 
+                    FROM FRIENDSOFFRIENDS
+                WHERE USERA = user1
+            ORDER BY COUNT(CALL getMutualFriends(user1, USERB) DESC;
+        OPEN cur;
+    END@
