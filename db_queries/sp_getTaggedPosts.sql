d1db9d412c9054a738b149051c975a286e4afcbb
durham-abric <durham.abric@mail.mcgill.ca>
2019-02-28 21:33:14 -0500

    Fix error in tagged posts query

--- db_queries/sp_getTaggedPosts.sql
+++ db_queries/sp_getTaggedPosts.sql
@@ -5,18 +5,20 @@ CREATE OR REPLACE PROCEDURE getTaggedPosts( IN user1 INTEGER,
     BEGIN
 
         DECLARE cur CURSOR WITH RETURN TO CALLER
-            FOR SELECT      *
+            FOR SELECT      p.*
                 FROM        Post as p
+                            JOIN (SELECT ownID, privacy FROM user) as u
+                            ON p.poster = u.ownID
                 WHERE       EXISTS( SELECT * 
                                 FROM    ContainsTag 
                                 WHERE   post = p.pID 
                                 AND     tag = tag1) 
-                AND         (privacy = "public" 
-                             OR (privacy = "friends"  
+                AND         (u.privacy = "public" 
+                             OR (u.privacy = "friends"  
                                 AND EXISTS( SELECT  * 
                                             FROM    BidirectionalFriends
                                             WHERE   USERA = user1
-                                            AND     USERB = p.poster)
+                                            AND     USERB = u.ownID)
                                 )   
                             )
                 ORDER BY    postDate DESC;
