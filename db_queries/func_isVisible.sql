19e9a3bea3ab5924dde822abd6a2219018d33294
durham-abric <durham.abric@mail.mcgill.ca>
2019-03-01 21:43:21 -0500

    Updates to SQL script formatting

--- db_queries/func_isVisible.sql
+++ db_queries/func_isVisible.sql
@@ -5,12 +5,13 @@ CREATE OR REPLACE FUNCTION isPostVisible( user1 INTEGER,
     NO EXTERNAL ACTION
 
     BEGIN
-      RETURN  SELECT  (CASE po.privacy
-                        WHEN po.privacy = 'private' THEN 0
-                        WHEN po.privacy = 'public'  THEN 1
-                        WHEN po.privacy = 'friends' THEN areFriends(user1, p.poster)
-                        ELSE -1
-                      END) AS visible
+
+      RETURN  SELECT  visible = (CASE po.privacy
+                                  WHEN po.privacy = 'private' THEN 0
+                                  WHEN po.privacy = 'public'  THEN 1
+                                  WHEN po.privacy = 'friends' THEN areFriends(user1, p.poster)
+                                  ELSE -1
+                                END)
               FROM Post AS p
               JOIN PageOwner AS po
               ON p.page = po.pageID
