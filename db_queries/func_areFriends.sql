65a04bc0e66740578544f364e5563eaa1cc2c716
durham-abric <durham.abric@mail.mcgill.ca>
2019-03-01 23:04:15 -0500

    Updates to SQL script formatting

--- db_queries/func_areFriends.sql
+++ db_queries/func_areFriends.sql
@@ -11,5 +11,6 @@ CREATE OR REPLACE FUNCTION areFriends(  user1 INTEGER,
                                          WHERE userA = user1
                                          AND userB = user2) THEN 1
                             ELSE 0
-                        END CASE;
+                        END CASE
+                END SELECT;
     END@
