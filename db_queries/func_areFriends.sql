28bab80a2fb6116b8db0144cddf51e9943fe8b69
durham-abric <durham.abric@mail.mcgill.ca>
2019-03-01 23:06:26 -0500

    Updates to SQL script formatting

--- db_queries/func_areFriends.sql
+++ db_queries/func_areFriends.sql
@@ -6,11 +6,10 @@ CREATE OR REPLACE FUNCTION areFriends(  user1 INTEGER,
 
     BEGIN
         RETURN  SELECT  CASE
-                            WHEN EXISTS( SELECT * 
-                                         FROM BidirectionalFriends
-                                         WHERE userA = user1
-                                         AND userB = user2) THEN 1
+                            WHEN COUNT(*) > 0 THEN 1
                             ELSE 0
                         END CASE
-                END SELECT;
+                FROM BidirectionalFriends 
+                WHERE userA = user1
+                AND userB = user2;
     END@
