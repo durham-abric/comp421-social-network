506cd04e3098aec8ed905d2092101c1a3ee38fba
durham-abric <durham.abric@mail.mcgill.ca>
2019-03-02 00:14:46 -0500

    Updates to SQL script formatting

--- db_queries/sp_getNewsFeed.sql
+++ db_queries/sp_getNewsFeed.sql
@@ -8,12 +8,12 @@ CREATE OR REPLACE PROCEDURE getNewsFeed(IN user1 INTEGER,
             FOR SELECT DISTINCT* 
                 FROM Post
                 WHERE EXISTS(   SELECT *
-                                FROM TABLE(getNewsFeedComments(user1) AS c
-                                WHERE pID = c.pID))
+                                FROM TABLE(getNewsFeedComments(user1)) AS c
+                                WHERE pID = c.pID)
                 OR EXISTS(      SELECT *
                                 FROM TABLE(getNewsFeedLikes(user1)) AS l
                                 WHERE pID = l.pID)
-                OR areFriends(user1, poster))
+                OR areFriends(user1, poster) = 1
                 ORDER BY postDate DESC
                 LIMIT numPosts;
 
